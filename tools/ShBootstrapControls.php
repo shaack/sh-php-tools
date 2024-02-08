592d938d25d1ae0c6d4a5cc6e2bc6f779be13165
shaack <shaack@gmail.com>
2024-02-08 16:35:16 +0100

    Update ShBootstrapControls.php

--- tools/ShBootstrapControls.php
+++ tools/ShBootstrapControls.php
@@ -19,26 +19,44 @@ class ShBootstrapControls
             </div>";
     }
 
-    public static function input($name, $label, $value, $placeholder = "", $type = "text", $size="", $required = true) {
+    public static function checkbox($name, $label, $value, $required = false)
+    {
         $attrRequired = $required ? "required" : "";
         return "
-        <div class='row gx-2'>
-            <label class='col-auto col-form-label' for='$name'>$label</label>
-            <div class='col-auto'>
-                <input size='$size' type='$type' $attrRequired class='form-control' id='$name' name='$name' placeholder='$placeholder' value='$value'>
-            </div>
+        <div class='form-check'>
+          <input class='form-check-input' $attrRequired type='checkbox' value='$value' id='$name' name='$name'>
+          <label class='form-check-label' for='$name'>
+            $label
+          </label>
         </div>";
     }
 
-    public static function select($name, $label, $options, $value, $required = true) {
-        $id = ShTools::createId(8);
+    public static function input($name, $label, $value, $placeholder = "", $type = "text", $size = "", $required = true)
+    {
+        $attrRequired = $required ? "required" : "";
+        $input = "<input size='$size' type='$type' $attrRequired class='form-control' id='$name' name='$name' placeholder='$placeholder' value='$value'>";
+        if ($label !== null) {
+            return "
+            <div class='row gx-2'>
+                <label class='col-auto col-form-label' for='$name'>$label</label>
+                <div class='col-auto'>
+                    $input
+                </div>
+            </div>";
+        } else {
+            return $input;
+        }
+    }
+
+    public static function select($name, $label, $options, $value, $required = true)
+    {
         $attrRequired = $required ? "required" : "";
         $optionsHtml = "";
         foreach ($options as $optionValue => $optionLabel) {
             $optionsHtml .= "<option value='$optionValue'>$optionLabel</option>";
         }
         $html = "";
-        if($label !== null) {
+        if ($label !== null) {
             $html .= "<label for='$name' class='form-label'>$label</label>";
         }
         $html .= "
