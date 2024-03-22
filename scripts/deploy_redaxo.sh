7ed86bb32a4ef77dc7be5b7717f165c20fbe266b
shaack <shaack@gmail.com>
2024-03-22 16:14:43 +0100

    before installation

--- scripts/deploy_redaxo.sh
+++ scripts/deploy_redaxo.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-project_dir=$(builtin cd ../../../; pwd)
-local_dir=$(builtin cd ../../../../../../../; pwd)
+project_dir=$(builtin cd ../../; pwd)
+local_dir=$(builtin cd ../../../../../../; pwd)
 source $project_dir/config.sh
 
 php_tools_folder=$local_dir/redaxo/src/addons/project/lib/sh-php-tools
 
 echo "⇨ redaxo deployment script"
+echo project_dir: $project_dir
+echo local_dir: $local_dir
+echo remote_dir: $remote_dir
 echo server: $server
 echo user: $user
-echo remote_dir: $remote_dir
-echo local_dir: $local_dir
 
 echo $php_tools_folder
 
-ssh -fMNS bgconn -o ControlPersist=yes $user@$server
-scp -ro ControlPath=bgconn $local_dir/redaxo/data/addons/developer $user@$server:$remote_dir/redaxo/data/addons
-mv -v $php_tools_folder/.git ~/Temp/
-scp -ro ControlPath=bgconn $local_dir/redaxo/src/addons/project $user@$server:$remote_dir/redaxo/src/addons
-mv -v ~/Temp/.git $php_tools_folder/
-scp -ro ControlPath=bgconn $local_dir/assets $user@$server:$remote_dir
-ssh -S bgconn -O exit -
+# ssh -fMNS bgconn -o ControlPersist=yes $user@$server
+# scp -ro ControlPath=bgconn $local_dir/redaxo/data/addons/developer $user@$server:$remote_dir/redaxo/data/addons
+# mv -v $php_tools_folder/.git ~/Temp/
+# scp -ro ControlPath=bgconn $local_dir/redaxo/src/addons/project $user@$server:$remote_dir/redaxo/src/addons
+# mv -v ~/Temp/.git $php_tools_folder/
+# scp -ro ControlPath=bgconn $local_dir/assets/local $user@$server:$remote_dir/assets
+# ssh -S bgconn -O exit -
 
