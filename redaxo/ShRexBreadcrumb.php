4dd9dbcfd91680bbb3a257de35deaf39029dba86
shaack <shaack@gmail.com>
2025-02-18 18:52:36 +0100

    Breadcrumb Extras Mode

--- redaxo/ShRexBreadcrumb.php
+++ redaxo/ShRexBreadcrumb.php
@@ -21,8 +21,13 @@ class ShRexBreadcrumb
         $pathArray = $this->article->getPathAsArray();
         $breadcrumbHtml = "";
         $i = 0;
+        $extrasMode = false;
         foreach ($pathArray as $pathId) {
             $pathArticle = rex_article::get($pathId);
+            if($pathArticle->getCategory()->getName() == "Extras") {
+                $extrasMode = true;
+                break;
+            }
             if ($i != 1) {
                 $breadcrumbHtml .= "<li class='breadcrumb-item'><a href='{$pathArticle->getUrl()}'>{$pathArticle->getName()}</a></li>";
             } else {
@@ -31,7 +36,9 @@ class ShRexBreadcrumb
             $i++;
         }
         // if article is different from the category, add the article to the breadcrumb
-        if ($this->article->getId() != $this->article->getCategoryId()) {
+        if($extrasMode) {
+            $breadcrumbHtml .= "<li class='breadcrumb-item'><a href='{$this->article->getUrl()}'>{$this->article->getName()}</a></li>";
+        } else if ($this->article->getId() != $this->article->getCategoryId()) {
             $breadcrumbHtml .= "<li class='breadcrumb-item'><a href='{$pathArticle->getUrl()}'>{$this->article->getName()}</a></li>";
         }
         return "<nav class='nav-breadcrumb' aria-label='breadcrumb'><ol class='breadcrumb'>$breadcrumbHtml</ol></nav>";
